83e22912e74de36a2ab91d5c47a51fc0e7659545
Gabriel Calixto <gabrielcalixtosoares@gmail.com>
2022-05-09 20:29:15 -0300

    Feito limite do valor das somas e não poder ser 0

--- Controllers/Controller.cs
+++ Controllers/Controller.cs
@@ -7,39 +7,58 @@ namespace PrimeiroExercicioAPI.Controllers
     public class PrimeiroController : ControllerBase
     {
         [HttpGet("ApiSoma")]
-        public double ApiDeSoma(double num1, double num2)
+        public IActionResult ApiDeSoma(double num1, double num2)
         {
-            return num1 + num2;
-        }
-            [HttpGet("ApiSubtracao")]
-        public double ApiDeSubtracao(double num1, double num2)
-        {
-            return num1 - num2;
-        }
-            [HttpGet("ApiMultiplicacao")]
-        public double ApiDeMultiplicacao(double num1, double num2)
-        {
-            return num1 * num2;
-        }
-            [HttpGet("ApiDivisao")]
-        public double ApiDeDivisao(double num1, double num2)
-        {
-            return num1 / num2;
-        }
-         
-     
-
-
-
-
-        [HttpGet("segundaApi")]
-          public IActionResult ErroDeRequest(int entrada)
-        {
-            if(entrada == 1)
+            if (num1 > 999 || num2 > 999)
             {
-                return BadRequest("1- Não é uma entrada válida");
+               return BadRequest("Numero muito grande para conta");                       
+            }else if (num1 == 0 || num2 == 0) 
+            {
+                return BadRequest("O valor não pode ser 0");     
             }
-            return Ok(entrada);
+
+            return Ok(num1 + num2);               
         }
+
+        [HttpGet("ApiSubtracao")]
+        public IActionResult ApiDeSubtracao(double num1, double num2)
+        {
+            if (num1 > 999 || num2 > 999)
+            {
+               return BadRequest("Numero muito grande para conta");                       
+            }else if (num1 == 0 || num2 == 0) 
+            {
+                return BadRequest("O valor não pode ser 0");     
+            }
+
+            return Ok(num1 - num2);               
+        }
+        [HttpGet("ApiMultiplicacao")]
+        public IActionResult ApiDeMultiplicacao(double num1, double num2)
+        {
+            if (num1 > 999 || num2 > 999)
+            {
+               return BadRequest("Numero muito grande para conta");                       
+            }else if (num1 == 0 || num2 == 0) 
+            {
+                return BadRequest("O valor não pode ser 0");     
+            }
+
+            return Ok(num1 * num2);               
+        }
+        [HttpGet("ApiDivisao")]
+        public IActionResult ApiDeDivisao(double num1, double num2)
+        {
+            if (num1 > 999 || num2 > 999)
+            {
+               return BadRequest("Numero muito grande para conta");                       
+            }else if (num1 == 0 || num2 == 0) 
+            {
+                return BadRequest("O valor não pode ser 0");     
+            }
+
+            return Ok(num1 / num2);               
+        }
+      
     }
 }
