cc191ce8c5bb3491b2d428f22826ec1560be8aef
Gabriel Calixto <gabrielcalixtosoares@gmail.com>
2022-05-09 20:12:52 -0300

    Feito Controller e contas matemáticas básicas

--- Controllers/Controller.cs
+++ Controllers/Controller.cs
@@ -1,7 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
 namespace PrimeiroExercicioAPI.Controllers
 {
-    public class Controller
+    [ApiController]
+    [Route("MinhaController")]
+
+    public class PrimeiroController : ControllerBase
     {
-        
+        [HttpGet("ApiSoma")]
+        public double ApiDeSoma(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+            [HttpGet("ApiSubtracao")]
+        public double ApiDeSubtracao(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+            [HttpGet("ApiMultiplicacao")]
+        public double ApiDeMultiplicacao(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+            [HttpGet("ApiDivisao")]
+        public double ApiDeDivisao(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+         
+     
+
+
+
+
+        [HttpGet("segundaApi")]
+          public IActionResult ErroDeRequest(int entrada)
+        {
+            if(entrada == 1)
+            {
+                return BadRequest("1- Não é uma entrada válida");
+            }
+            return Ok(entrada);
+        }
     }
 }
