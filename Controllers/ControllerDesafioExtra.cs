bd280d5fc101aedbe2fee25f268229a08d2bff4b
Gabriel Calixto <gabrielcalixtosoares@gmail.com>
2022-05-09 21:00:16 -0300

    Feito APIS chamando metodos das contas

--- Controllers/ControllerDesafioExtra.cs
+++ Controllers/ControllerDesafioExtra.cs
@@ -5,10 +5,25 @@ namespace PrimeiroExercicioAPI.Controllers
     public class ControllerDesafioExtra : Controller
     {
         [HttpGet("ApiCalculadora")]
-        public IActionResult ApiDeCalculadora(double num1, double num2)
+        public IActionResult ApiDeSoma(double num1, double num2)
         {   
             var calc = new Calculadora();
             return calc.Somar(num1, num2);
+        }
+         public IActionResult ApiDeSubtracao(double num1, double num2)
+        {   
+            var calc = new Calculadora();
+            return calc.Subtrair(num1, num2);
+        }
+         public IActionResult ApiDeMultiplicacao(double num1, double num2)
+        {   
+            var calc = new Calculadora();
+            return calc.Multiplicar(num1, num2);
+        }
+         public IActionResult ApiDeDivisao(double num1, double num2)
+        {   
+            var calc = new Calculadora();
+            return calc.Dividir(num1, num2);
         }
     }
 
